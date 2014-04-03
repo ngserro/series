ba9eb90c1767e9b7fcf3aeb7f3224c44a1edb074
Nuno Serro <ngserro@gmail.com>
2014-04-03 16:46:07 +0100

    Fixed extension parsing. Folders are no longer deleted.

--- series_helper.sh
+++ series_helper.sh
@@ -250,10 +250,7 @@ case "$1" in
 				
 				#Move ficheiros para a raiz e apaga pastas
  				find . -iregex '.*\(srt\|avi\|mkv\|mpg\|mpeg\|mp4\|m4v\|wmv\)' -exec mv "{}" . \; 2> /dev/null
- 				find . -type d -exec rm -rf "{}" \; 2> /dev/null
-
-				ficheiros=*
-				
+		
 				echo "################" >> $log
 				date +'%d-%m-%Y-%H:%M' >> $log
 				echo "################" >> $log
@@ -262,7 +259,7 @@ case "$1" in
 				echo "Extensoes filtradas: *.srt *.avi *.mkv *.mpg *.mpeg *.mp4 *.m4v *.wmv" >> $log
 				echo "Comandos utilizados:" >> $log
 
-				for item in $ficheiros
+				for item in *.srt *.avi *.mkv *.mpg *.mpeg *.mp4 *.m4v *.wmv
 				do
 					#Renomeia
 					novo_nome=`$bin_path/series.rb "$item"` 
