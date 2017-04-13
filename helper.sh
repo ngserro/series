ac6b7979ca84222786c290edf0e3543ecb0cfbd2
Nuno Serro <ngserro@gmail.com>
2017-04-13 18:50:17 +0100

    Changed html font style

--- helper.sh
+++ helper.sh
@@ -41,6 +41,12 @@ case "$1" in
 		template_beginning="
 		<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
 		<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"pt\" lang=\"pt\">
+			<style> 
+				  html *
+				{
+				   font-family: Arial;
+				}
+			</style>
 			<head>
 				<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
 				<title>TV Shows</title>
